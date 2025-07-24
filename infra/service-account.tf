ade187629a708c234e4b8cd91e4115a0ba7d0a6d
Aleksandar Srajer <aleksandassrajer@gmail.com>
2025-07-24 13:49:49 +0200

    Remote terraform backend

--- infra/service-account.tf
+++ infra/service-account.tf
@@ -34,3 +34,9 @@ resource "google_project_iam_member" "github_actions_sa" { //Dodeljuje ulogu ser
   role    = "roles/iam.serviceAccountUser"
   member  = "serviceAccount:${google_service_account.github_actions.email}"
 }
+
+resource "google_storage_bucket_iam_member" "github_actions_storage" {
+  bucket = "cartwish-terraform-state"
+  role   = "roles/storage.objectAdmin"
+  member = "serviceAccount:${google_service_account.github_actions.email}"
+}
