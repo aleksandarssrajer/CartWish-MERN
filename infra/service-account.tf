b405d4083b573a6cf6c0c260506d9abd20e017d8
Aleksandar Srajer <aleksandassrajer@gmail.com>
2025-07-24 10:28:30 +0200

    Ajde brate molim te

--- infra/service-account.tf
+++ infra/service-account.tf
@@ -29,9 +29,11 @@ resource "google_project_iam_member" "github_actions_token" {
   member  = "serviceAccount:${google_service_account.github_actions.email}"
 }
 
+
 resource "google_project_iam_member" "github_actions_sa" { //Dodeljuje ulogu service account user-a nasem service accountu ovo nam daje da GitHub Actions putem Workload Identity Federetaion-a preuzme identitet ovog servis account-a
   project = "sara-sandbox-interns"
   role    = "roles/iam.serviceAccountUser"
   member  = "serviceAccount:${google_service_account.github_actions.email}"
 }
-#####
+########
+////
