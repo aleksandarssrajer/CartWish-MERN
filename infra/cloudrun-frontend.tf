ebf6734c0b0248f3fdb095b633e31488c8fb6d46
Aleksandar Srajer <aleksandassrajer@gmail.com>
2025-07-24 13:45:30 +0200

    Remote terraform backend

--- infra/cloudrun-frontend.tf
+++ infra/cloudrun-frontend.tf
@@ -2,11 +2,11 @@ resource "google_cloud_run_service" "frontend" {
   name     = "cartwish-frontend"
   location = "europe-west1"
 
-  depends_on = [google_service_account.frontend_sa]
+  # depends_on = [google_service_account.frontend_sa]
 
   template {
     spec {
-      service_account_name = google_service_account.frontend_sa.email
+      # service_account_name = google_service_account.frontend_sa.email
       containers {
         image = "europe-west1-docker.pkg.dev/sara-sandbox-interns/cartwish-repo/frontend:latest"
         ports {
@@ -18,17 +18,17 @@ resource "google_cloud_run_service" "frontend" {
   
 }
 
-resource "google_service_account" "frontend_sa" {
-  account_id   = "frontend-sa"
-  display_name = "Cloud Run frontend SA"
-}
+# resource "google_service_account" "frontend_sa" {
+#   account_id   = "frontend-sa"
+#   display_name = "Cloud Run frontend SA"
+# }
 
 
-resource "google_cloud_run_service_iam_member" "frontend_public" {
-  location = google_cloud_run_service.frontend.location
-  service  = google_cloud_run_service.frontend.name
-  role     = "roles/run.invoker"
-  member   = "allUsers"
-}
+# resource "google_cloud_run_service_iam_member" "frontend_public" {
+#   location = google_cloud_run_service.frontend.location
+#   service  = google_cloud_run_service.frontend.name
+#   role     = "roles/run.invoker"
+#   member   = "allUsers"
+# }
 
  
