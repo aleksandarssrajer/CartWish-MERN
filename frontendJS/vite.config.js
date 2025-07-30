1afcd807ac4b6ed430ba1a55225ac4358369b8a8
Aleksandar Srajer <aleksandassrajer@gmail.com>
2025-07-30 11:01:41 +0200

    Ajde pliz

--- frontendJS/vite.config.js
+++ frontendJS/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/cartwish-frontend-bucket-sara-sandbox-interns/',
+  base: process.env.VITE_BASE_PATH || '/',
   plugins: [react()],
 })
