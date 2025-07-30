c9ee8c620566de44d8046a132e12e082ea4220e8
Aleksandar Srajer <aleksandassrajer@gmail.com>
2025-07-30 13:56:52 +0200

    Pliz

--- infra/modules/frontend/load-balancer.tf
+++ infra/modules/frontend/load-balancer.tf
@@ -1,11 +1,12 @@
 resource "google_compute_global_address" "frontend_ip" {
     name = "frontend-static-ip"
+    project = var.project_id
 }
 
 resource "google_compute_managed_ssl_certificate" "frontend_ssl_cert" {
     name = "frontend-ssl-cert"
     managed {
-        domains = [var.domain_name]  # Replace with your domain
+        domains = [var.domain_name]  
     }
 }
 
@@ -25,11 +26,11 @@ resource "google_compute_url_map" "frontend_url_map" {
 
   path_matcher {
     name            = "allpaths"
-    default_service = google_compute_backend_bucket.frontend_backend_bucket.id
+    default_service = google_compute_backend_bucket.frontend_backend_bucket.self_link
 
     path_rule {
       paths   = ["/*"]
-      service = google_compute_backend_bucket.frontend_backend_bucket.id
+      service = google_compute_backend_bucket.frontend_backend_bucket.self_link
     }
   }
 }
@@ -43,8 +44,8 @@ resource "google_compute_target_https_proxy" "frontend_https_proxy" {
 
 resource "google_compute_global_forwarding_rule" "frontend_forwarding_rule" {
   name        = "frontend-https-forwarding-rule"
-  ip_address  = google_compute_global_address.frontend_ip.id
+  ip_address  = google_compute_global_address.frontend_ip.self_link
   ip_protocol = "TCP"
   port_range  = "443"
-  target      = google_compute_target_https_proxy.frontend_https_proxy.id
+  target      = google_compute_target_https_proxy.frontend_https_proxy.self_link
 }
