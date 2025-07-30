5feaf97169fea2bd6cb295b32d3cd0d97ca27adc
Aleksandar Srajer <aleksandassrajer@gmail.com>
2025-07-30 11:11:21 +0200

    Ajde sad

--- infra/modules/frontend/main.tf
+++ infra/modules/frontend/main.tf
@@ -32,7 +32,7 @@ resource "google_storage_bucket" "frontend_bucket" {
 
   website {
     main_page_suffix = "index.html"
-    not_found_page   = "404.html"
+    not_found_page   = "index.html"
   }
 
    cors {
