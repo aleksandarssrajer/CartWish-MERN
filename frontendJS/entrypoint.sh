2d0d76f75932c315026133ee51a57dc51d650a53
Aleksandar Srajer <aleksandassrajer@gmail.com>
2025-08-05 14:41:59 +0200

    Molim te

--- frontendJS/entrypoint.sh
+++ frontendJS/entrypoint.sh
@@ -1,4 +1,21 @@
 #!/bin/sh
-# Dodati su \" pre i posle varijable da bi se URL upisao kao string
-sed -i "s#%%VITE_API_URL%%#\"$VITE_API_URL\"#g" /usr/share/nginx/html/config.js
+
+echo "--- Entrypoint skripta je pokrenuta ---"
+echo "Primljena VITE_API_URL vrednost: '$VITE_API_URL'"
+
+# Proveravamo da li fajl postoji pre izmene
+if [ -f /usr/share/nginx/html/config.js ]; then
+    echo "Fajl config.js pronađen. Vrši se zamena placeholdera."
+    # Koristimo # kao delimiter da izbegnemo probleme sa kosim crtama u URL-u
+    # Dodajemo navodnike oko varijable
+    sed -i "s#%%VITE_API_URL%%#\"$VITE_API_URL\"#g" /usr/share/nginx/html/config.js
+else
+    echo "GREŠKA: Fajl config.js NIJE pronađen na putanji /usr/share/nginx/html/config.js"
+fi
+
+echo "--- Sadržaj fajla config.js NAKON izmene ---"
+cat /usr/share/nginx/html/config.js
+echo "---------------------------------------------"
+
+echo "Pokrećem Nginx..."
 exec nginx -g 'daemon off;'
