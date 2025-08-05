85eb1161ec3245f89a48c91429b520edcf7c720e
Aleksandar Srajer <aleksandassrajer@gmail.com>
2025-08-05 14:37:34 +0200

    Valjda ce sad raditi

--- frontendJS/entrypoint.sh
+++ frontendJS/entrypoint.sh
@@ -1,10 +1,4 @@
 #!/bin/sh
-# frontendJS/entrypoint.sh
-
-# Nađi placeholder u config.js i zameni ga sa vrednošću iz env varijable
-# Koristimo duple navodnike oko "$VITE_API_URL" da bi se varijabla expandovala
-# Koristimo drugi delimiter za sed (recimo #) jer URL može sadržati /
+# Dodati su \" pre i posle varijable da bi se URL upisao kao string
 sed -i "s#%%VITE_API_URL%%#\"$VITE_API_URL\"#g" /usr/share/nginx/html/config.js
-
-# Pokreni originalnu komandu (Nginx)
 exec nginx -g 'daemon off;'
