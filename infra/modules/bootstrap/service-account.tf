b8cca32af511f510f1a2a8c30e975ecfef7e2b2f
Aleksandar Srajer <aleksandassrajer@gmail.com>
2025-08-05 14:20:44 +0200

    Testing perrmision

--- infra/modules/bootstrap/service-account.tf
+++ infra/modules/bootstrap/service-account.tf
@@ -11,7 +11,9 @@ resource "google_project_iam_member" "github_deploy_permissions" {
     "roles/iam.serviceAccountUser",
     "roles/viewer",
     "roles/iam.workloadIdentityPoolAdmin",
-    "roles/iam.serviceAccountTokenCreator"
+    "roles/iam.serviceAccountTokenCreator",
+    "roles/resourcemanager.projectIamAdmin",
+    "roles/iam.serviceAccountAdmin"
   ])
 
   role    = each.key
@@ -19,45 +21,3 @@ resource "google_project_iam_member" "github_deploy_permissions" {
   member  = "serviceAccount:${google_service_account.github_actions.email}"
 }
 
-# resource "google_project_iam_member" "github_actions_cloud_run" {
-#   role    = "roles/run.admin"
-#   member  = "serviceAccount:${google_service_account.github_actions.email}"
-#   project = var.project_id
-# }
-
-# resource "google_project_iam_member" "github_actions_artifact" {
-#   role    = "roles/artifactregistry.writer"
-#   member  = "serviceAccount:${google_service_account.github_actions.email}"
-#   project = var.project_id
-# }
-
-# resource "google_project_iam_member" "github_actions_viewer" {
-#   role    = "roles/viewer"
-#   member  = "serviceAccount:${google_service_account.github_actions.email}"
-#   project = var.project_id
-# }
-
-# resource "google_project_iam_member" "github_actions_workload_identity_admin" {
-#   role    = "roles/iam.workloadIdentityPoolAdmin"
-#   member  = "serviceAccount:${google_service_account.github_actions.email}"
-#   project = var.project_id
-# }
-
-# resource "google_project_iam_member" "github_actions_token" {
-#   role    = "roles/iam.serviceAccountTokenCreator"
-#   member  = "serviceAccount:${google_service_account.github_actions.email}"
-#   project = var.project_id
-# }
-
-# resource "google_project_iam_member" "github_actions_sa" {
-#   role    = "roles/iam.serviceAccountUser"
-#   member  = "serviceAccount:${google_service_account.github_actions.email}"
-#   project = var.project_id
-# }
-
-# resource "google_storage_bucket_iam_member" "github_actions_storage" {
-#   bucket = "cartwish-terraform-state"
-#   role   = "roles/storage.objectAdmin"
-#   member = "serviceAccount:${google_service_account.github_actions.email}"
-
-# }
